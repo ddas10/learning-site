c7b12d6ceff46346cacc884154780c691601a1f4
Dhyan Das <dhyanannhicd@adobe.com>
2023-11-17 15:05:58 +0900

    change text

--- blocks/country/country.js
+++ blocks/country/country.js
@@ -1,27 +1,28 @@
 export default function decorate(block) {
 
-    // fetch('https://countries.trevorblades.com/', {
-    //   method: 'POST',
-    //   body: JSON.stringify({
-    //     query: `query GetCountry {
-    //       country(code: $ID)
-    //       name
-    //     }`,
-    //     variables: {
-    //       ID: 'BR'
-    //     }
-    //   }),
-    //   headers: {
-    //       'content-type': 'application/json'
-    //   }
-    // }).then(async (data) => {
-    //     // Console log our return data
-    //     console.log(await data.json());
-    // });
+    let res;
+    fetch('https://countries.trevorblades.com/', {
+      method: 'POST',
+      body: JSON.stringify({
+        query: `query {
+          country(code: $ID)
+          name
+        }`,
+        variables: {
+          ID: 'BR'
+        }
+      }),
+      headers: {
+          'content-type': 'application/json'
+      }
+    })
+    .then(res => res.json()) 
+    .then(data => {
+        res = data.data.country.name;
+    });
     
     const productDiv = block.querySelector(':scope > div > div');
-    // const p = document.createElement('p');
-    productDiv.innerText = 'Testing';
+    productDiv.innerText = res;
 
 }
 
